c459dea119458c2e411a9fee2e6430345d41ef6c
kmax3ever <kmax3ever2@gmail.com>
2023-11-25 22:52:51 +0700

    update deploy polygon

--- script/deployParameters/DeployPolygon.s.sol
+++ script/deployParameters/DeployPolygon.s.sol
@@ -7,18 +7,18 @@ import {RouterParameters} from 'contracts/base/RouterImmutables.sol';
 contract DeployPolygon is DeployUniversalRouter {
     function setUp() public override {
         params = RouterParameters({
-            permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
+            permit2: 0xAab0f6c1a793430B553c1af66A68e29e22ab23EE,
             weth9: 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270,
-            seaportV1_5: 0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC,
+            seaportV1_5: UNSUPPORTED_PROTOCOL,
             steth: UNSUPPORTED_PROTOCOL,
             wsteth: UNSUPPORTED_PROTOCOL,
-            seaportV1_4: 0x00000000000001ad428e4906aE43D8F9852d0dD6,
-            openseaConduit: 0x1E0049783F008A0085193E00003D00cd54003c71,
+            seaportV1_4: UNSUPPORTED_PROTOCOL,
+            openseaConduit: UNSUPPORTED_PROTOCOL,
             nftxZap: UNSUPPORTED_PROTOCOL,
             x2y2: UNSUPPORTED_PROTOCOL,
             foundation: UNSUPPORTED_PROTOCOL,
             sudoswap: UNSUPPORTED_PROTOCOL,
-            elementMarket: 0xEAF5453b329Eb38Be159a872a6ce91c9A8fb0260,
+            elementMarket: UNSUPPORTED_PROTOCOL,
             nft20Zap: UNSUPPORTED_PROTOCOL,
             cryptopunks: UNSUPPORTED_PROTOCOL,
             looksRareV2: UNSUPPORTED_PROTOCOL,
@@ -31,6 +31,6 @@ contract DeployPolygon is DeployUniversalRouter {
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
         });
 
-        unsupported = 0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B;
+        unsupported = 0x8cEe3440e6F606326a4BF58FCb573b9bFCAb3CD6;
     }
 }
