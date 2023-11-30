a779ad190a971b41171f8186e963cc16dd63c89b
kmax3ever <kmax3ever2@gmail.com>
2023-12-01 00:00:57 +0700

    update deploy polygon

--- script/deployParameters/DeployPolygon.s.sol
+++ script/deployParameters/DeployPolygon.s.sol
@@ -25,8 +25,8 @@ contract DeployPolygon is DeployUniversalRouter {
             routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareToken: UNSUPPORTED_PROTOCOL,
-            v2Factory: 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f,
-            v3Factory: 0x1F98431c8aD98523631AE4a59f267346ea31F984,
+            v2Factory: 0x843bed96dB8b6F9f01e824aC2c8C3eB832b09Ce8,
+            v3Factory: 0x6F210f6079a2ef18c278B4d796B8Fd366b9fe08c,
             pairInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
         });
